f5c33d928f42dfbfa078bf221dc7d6916012af39
eva.jilkova <viktorinova.eva@gmail.com>
2024-12-10 05:15:37 +0100

    menu with styles not working

--- src/main/webapp/app/entities/menu.tsx
+++ src/main/webapp/app/entities/menu.tsx
@@ -1,12 +1,57 @@
 import React from 'react';
+import { useState } from 'react';
+import styles from '../modules/home/style.css';
 
-const EntitiesMenu = () => {
+function EntitiesMenu() {
+  // adding the states
+  const [isActive, setIsActive] = useState(false);
+  //add the active class
+  const toggleActiveClass = () => {
+    setIsActive(!isActive);
+  };
+  //clean up function to remove the active class
+  const removeActive = () => {
+    setIsActive(false);
+  };
   return (
-    <>
-      {/* prettier-ignore */}
-      {/* jhipster-needle-add-entity-to-menu - JHipster will add entities to the menu here */}
-    </>
+    <div className="App">
+      <header className="App-header">
+        <nav className={`${styles.navbar}`}>
+          {/* logo */}
+          <a href="#home" className={`${styles.logo}`}>
+            Dev.{' '}
+          </a>
+          <ul className={`${styles.navMenu} ${isActive ? styles.active : ''}`}>
+            <li onClick={removeActive}>
+              <a href="#home" className={`${styles.navLink}`}>
+                Home
+              </a>
+            </li>
+            <li onClick={removeActive}>
+              <a href="#home" className={`${styles.navLink}`}>
+                Catalog
+              </a>
+            </li>
+            <li onClick={removeActive}>
+              <a href="#home" className={`${styles.navLink}`}>
+                All products
+              </a>
+            </li>
+            <li onClick={removeActive}>
+              <a href="#home" className={`${styles.navLink}`}>
+                Contact
+              </a>
+            </li>
+          </ul>
+          <div className={`${styles.hamburger} ${isActive ? styles.active : ''}`} onClick={toggleActiveClass}>
+            <span className={`${styles.bar}`}></span>
+            <span className={`${styles.bar}`}></span>
+            <span className={`${styles.bar}`}></span>
+          </div>
+        </nav>
+      </header>
+    </div>
   );
-};
+}
 
 export default EntitiesMenu;
