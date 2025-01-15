360c1fafcf88ff0f28869db1f45d4e3a58a04d2f
eva.jilkova <viktorinova.eva@gmail.com>
2025-01-15 01:31:55 +0100

    Itinerary menu as drop down

--- src/main/webapp/app/shared/layout/menus/itinerary.tsx
+++ src/main/webapp/app/shared/layout/menus/itinerary.tsx
@@ -1,8 +1,20 @@
 import React from 'react';
 import MenuItem from 'app/shared/layout/menus/menu-item';
+import { NavDropdown } from 'app/shared/layout/menus/menu-components';
+
+const itineraryMenuItems = () => (
+  <>
+    <MenuItem id="login-item" icon="sign-in-alt" to="/itinerary" data-cy="login">
+      <span>Public Itinerary</span>
+    </MenuItem>
+    <MenuItem icon="user-plus" to="/itinerary" data-cy="register">
+      <span>Personal Itinerary</span>
+    </MenuItem>
+  </>
+);
 
 export const ItineraryMenu = () => (
-  <MenuItem to="/itinerary" icon={'function'}>
-    <span>Itinerary</span>
-  </MenuItem>
+  <NavDropdown icon="user" name="Itinerary" id="itinerary-menu" data-cy="accountMenu">
+    {itineraryMenuItems()}
+  </NavDropdown>
 );
